cbf80732885db998f4bb2ae5319ce1d08e3abe90
HP-ZBOOK-05 <HP-ZBOOK-05@192.168.150.64>
2018-12-06 18:52:19 +0100

    Can read a simple table in wikipedia and convert it in a CSV file

--- wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
+++ wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
@@ -1,13 +1,24 @@
 package fr.univrennes1.istic.wikipediamatrix;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args ) throws IOException
+	{
+		Html pageCanon = new Html();
+		pageCanon.init("https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras");
+		Tableau tab = pageCanon.getTableau(0);
+		tab.genererCSV("output/html/test3.csv");
+	}
 }
