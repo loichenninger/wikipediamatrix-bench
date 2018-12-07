89b298f05e3b18c679d003fb37902575d23f5381
HP-ZBOOK-05 <HP-ZBOOK-05@192.168.150.64>
2018-12-07 11:24:34 +0100

    Added url verification and wikitable sortable selection

--- wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
+++ wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
@@ -1,12 +1,9 @@
 package fr.univrennes1.istic.wikipediamatrix;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Hello world!
@@ -16,9 +13,22 @@ public class App
 {
 	public static void main( String[] args ) throws IOException
 	{
-		Html pageCanon = new Html();
-		pageCanon.init("https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras");
-		Tableau tab = pageCanon.getTableau(0);
-		tab.genererCSV("output/html/test3.csv");
+		String url="Comparison_of_Norwegian_Bokmål_and_Standard_Danish";
+		Url chemin = new Url();
+		chemin.setChemin("https://en.wikipedia.org/wiki/"+url);
+		System.out.println(chemin.estValide());
+//		Html pageCanon = new Html();
+//		pageCanon.init("https://en.wikipedia.org/wiki/"+url);
+//		pageCanon.extractTableaux();
+//		int i = 1;
+//		for (Tableau tab : pageCanon.getTableaux()) {
+//			String csvFileName = mkCSVFileName(url,i);
+//			tab.genererCSV("output/html/"+csvFileName);
+//			i++;
+//		}
+	}
+	
+	private static String mkCSVFileName(String url, int n) {
+		return url.trim() + "-" + n + ".csv";
 	}
 }
--- wikimatrix/src/test/java/fr/univrennes1/istic/wikipediamatrix/BenchTest.java
+++ wikimatrix/src/test/java/fr/univrennes1/istic/wikipediamatrix/BenchTest.java
@@ -15,16 +15,16 @@ import org.junit.Test;
  */
 
 public class BenchTest {
-	
+
 	/*
-	*  the challenge is to extract as many relevant tables as possible
-	*  and save them into CSV files  
-	*  from the 300+ Wikipedia URLs given
-	*  see below for more details
-	**/
+	 *  the challenge is to extract as many relevant tables as possible
+	 *  and save them into CSV files  
+	 *  from the 300+ Wikipedia URLs given
+	 *  see below for more details
+	 **/
 	@Test
 	public void testBenchExtractors() throws Exception {
-		
+
 		String BASE_WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/";
 		// directory where CSV files are exported (HTML extractor) 
 		String outputDirHtml = "output" + File.separator + "html" + File.separator;
@@ -32,40 +32,61 @@ public class BenchTest {
 		// directory where CSV files are exported (Wikitext extractor) 
 		String outputDirWikitext = "output" + File.separator + "wikitext" + File.separator;
 		assertTrue(new File(outputDirWikitext).isDirectory());
-		
+
 		File file = new File("inputdata" + File.separator + "wikiurls.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file));
-	    String url;
-	    int nurl = 0;
-	    while ((url = br.readLine()) != null) {
-	       String wurl = BASE_WIKIPEDIA_URL + url; 
-	       System.out.println("Wikipedia url: " + wurl);
-	       // TODO: do something with the Wikipedia URL 
-	       // (ie extract relevant tables for correct URL, with the two extractors)
-		    
-	       
-	       // for exporting to CSV files, we will use mkCSVFileName 
-	       // example: for https://en.wikipedia.org/wiki/Comparison_of_operating_system_kernels
-	       // the *first* extracted table will be exported to a CSV file called 
-	       // "Comparison_of_operating_system_kernels-1.csv"
-	       String csvFileName = mkCSVFileName(url, 1);
-	       System.out.println("CSV file name: " + csvFileName);
-	       // the *second* (if any) will be exported to a CSV file called
-	       // "Comparison_of_operating_system_kernels-2.csv"
+		String url;
+		int nurl = 0;
+		while ((url = br.readLine()) != null) {
+			String wurl = BASE_WIKIPEDIA_URL + url; 
+			System.out.println("Wikipedia url: " + wurl);
+			// TODO: do something with the Wikipedia URL 
+			// (ie extract relevant tables for correct URL, with the two extractors)
+			Url chemin = new Url();
+			chemin.setChemin(wurl);
+			if (chemin.estValide()) {
+				Html page = new Html();
+				page.init(wurl);
+				page.extractTableaux();
+				int i = 1;
+				for (Tableau tab : page.getTableaux()) {
+					String csvFileName = mkCSVFileName(url,i);
+					tab.genererCSV("output/html/"+csvFileName);
+					i++;
+				}
 
-	       
-	       // TODO: the HTML extractor should save CSV files into output/HTML
-	       // see outputDirHtml 
-	       
-	       // TODO: the Wikitext extractor should save CSV files into output/wikitext
-	       // see outputDirWikitext      
-	       
-	       nurl++;	       
-	    }
-	    
-	    br.close();	    
-	    assertEquals(nurl, 336);
-	    
+				// for exporting to CSV files, we will use mkCSVFileName 
+				// example: for https://en.wikipedia.org/wiki/Comparison_of_operating_system_kernels
+				// the *first* extracted table will be exported to a CSV file called 
+				// "Comparison_of_operating_system_kernels-1.csv"
+
+				// the *second* (if any) will be exported to a CSV file called
+				// "Comparison_of_operating_system_kernels-2.csv"
+
+
+				// TODO: the HTML extractor should save CSV files into output/HTML
+				// see outputDirHtml 
+
+				// TODO: the Wikitext extractor should save CSV files into output/wikitext
+				// see outputDirWikitext   
+
+			}
+			nurl++;	       
+		}
+		//		for (int i=0;i<10;i++) {
+		//			url = br.readLine();
+		//			String wurl = BASE_WIKIPEDIA_URL + url;
+		//			System.out.println("Wikipedia url: " + wurl);
+		//			Html page = new Html();
+		//			page.init(wurl);
+		//			Tableau tab = page.getTableau(0);
+		//			String csvFileName = mkCSVFileName(url,1);
+		//			tab.genererCSV("output/html/"+csvFileName);
+		//		}
+
+		br.close();	    
+		assertEquals(nurl, 336);
+
 
 
 
